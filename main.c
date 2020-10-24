eba76c3dbd2f7680ec542b3a46dbabe96df41aeb
systemad <dannepuke@gmail.com>
2020-10-24 23:23:37 +0200

    Dellupgift 1 progress

--- main.c
+++ main.c
@@ -1,19 +1,35 @@
 #include <avr/io.h>
 #include <avr/pgmspace.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
 
 #include <stdio.h>
 
-#include "led.h"
+#include "led.h" // TODO: Add led function code
 #include "serial.h"
 #include "timer.h"
 
+uint16_t time = 0;
+
 void main (void) {
 	uart_init();
+	timer_init();
+	DDRB |= (1<<PB3);
 
-	while (1) {
-		/* remove this once you've verified it works */
-		printf_P(PSTR("Hello there\n"));
-		_delay_ms(1000);
-	}
+	// 
+	TCCR0A = 0;
+
+	sei();
+	while (1) {};
+
+}
+
+
+ISR(TIMER0_COMPA_vect){
+	time++;
+	// TODO: Wrong value?
+	if (time > 100) {
+        PORTB ^= (1<<PB3);
+        time = 0;       
+    }
 }
--- timer.c
+++ timer.c
@@ -2,7 +2,21 @@
 
 #include "timer.h"
 
+// TODO: Find right prescale value
+const uint16_t tl_compare = 1562; // 15620 //.5; // Prescaler 1024
+
 void timer_init() {
-	// ...
+
+	// Set timer0 to CTC mode
+	TCCR0A = (1 << WGM01) ;
+	
+	// Set prescaler to 1024
+	TCCR0B = (1 << CS02); 
+
+	OCR0A = tl_compare;
+    TIMSK0 = (1 << OCIE1A);
+
 }
+
+
 
