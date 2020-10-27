9d55aecff2b013b86a08c303ae9e19fb9bcefd9d
systemad <dannepuke@gmail.com>
2020-10-27 14:17:28 +0100

    Small changes

--- main.c
+++ main.c
@@ -17,14 +17,5 @@ void main (void) {
 	timer_init();	// Initialize timer
 	LED_init();		// Initialize LED pin(s)
 
-	while (1) {						
-		if(TCNT0 >= tl_compare){	// If timer(TCNTO) matches compare value
-			time++;					// Increment value by 1
-			TCNT0 = 0;				// And reset timer
-			if(time > 10){			
-				PORTB ^= (1 << PB3);	// Toggle LED and reset time variable if time goes above 10	
-				time = 0;
-			}
-		}
-	}
+	while (1) {}
 }
--- timer.c
+++ timer.c
@@ -6,15 +6,18 @@ void timer_init() {
 
 	// Fast PWN Mode
 	TCCR0A |= (1 << WGM01) | (1 << WGM00);
+
+	// set non-inverting mode
+	TCCR0A |= (1 << COM0A1);
 	
 	// Set prescaler to 1024
-	TCCR0B |= (1 << CS02 ) | ( 1 << CS00);
+	//TCCR0B |= (1 << CS02 ) | ( 1 << CS00);
 
 	// Reset timer
 	TCNT0 = 0;
 
-	// Set compate value
-	OCR0A = tl_compare;
+	
+	OCR0A = 128;
 
 }
 
